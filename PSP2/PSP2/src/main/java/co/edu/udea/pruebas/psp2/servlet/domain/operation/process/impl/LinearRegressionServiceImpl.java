fdbcdf803ee962280204bf30c6f85802fee33693
liseth.rendon <liseth.rendon@lis-sala2>
2016-08-25 18:09:59 -0500

    Pruebas sin datos y raiz negativa

--- PSP2/PSP2/src/main/java/co/edu/udea/pruebas/psp2/servlet/domain/operation/process/impl/LinearRegressionServiceImpl.java
+++ PSP2/PSP2/src/main/java/co/edu/udea/pruebas/psp2/servlet/domain/operation/process/impl/LinearRegressionServiceImpl.java
@@ -17,40 +17,43 @@ import java.util.ArrayList;
  * @author raven
  */
 public class LinearRegressionServiceImpl implements LinearRegressionService {
-
     
     ArrayList<Double> salidas = new ArrayList();
-        
-    Double sumatoriaX =0.0;
-    Double sumatoriaY =0.0;
-    Double sumatoriaXCuad =0.0;
-    Double sumatoriaYCuad =0.0;
+    
+    Double sumatoriaX = 0.0;
+    Double sumatoriaY = 0.0;
+    Double sumatoriaXCuad = 0.0;
+    Double sumatoriaYCuad = 0.0;
     Double sumatoriaXY = 0.0;
     Double promedioX = 0.0;
     Double promedioY = 0.0;
-    int cantidad =0;
-    
+    int cantidad = 0;
+
     /**
-     * 
-     * Este método debe retornar un arreglo con los 4 valores que se piden para la regresión, 
-     * dichos valores son B0, B1, Rxy, R2 y DEBEN SER RETORNADOS EN ESE MISMO ORDEN DENTRO DEL ARREGLO
-     * 
-     * @param datos es la lista ligada que tiene los valores sobre los que se hará el cálculo, viene en forma de dupla.
+     *
+     * Este método debe retornar un arreglo con los 4 valores que se piden para
+     * la regresión, dichos valores son B0, B1, Rxy, R2 y DEBEN SER RETORNADOS
+     * EN ESE MISMO ORDEN DENTRO DEL ARREGLO
+     *
+     * @param datos es la lista ligada que tiene los valores sobre los que se
+     * hará el cálculo, viene en forma de dupla.
      * @return Un arreglo con los valores de B0, B1, Rxy, R2
-     * @throws PSP2Exception cuando existe un problema que imposibilite el cálculo de algun valor
+     * @throws PSP2Exception cuando existe un problema que imposibilite el
+     * cálculo de algun valor
      */
     @Override
     public double[] getLinearRegressionParameters(ListaLigadaConCabeza<Dupla<Double>> datos) throws PSP2Exception {
         
+        double[] resultado = null;
         /*
-        EJEMPLO DE COMO RECORRER EL ARREGLO DE DATOS:
-        */
+         EJEMPLO DE COMO RECORRER EL ARREGLO DE DATOS:
+         */
         sumatorias(datos);
         salidas.add(beta0());
         salidas.add(beta1());
         salidas.add(rxy());
         salidas.add(rCuadrado());
-         
+
         //Esto es una dupla, almacena 2 objetos del tipo de dato que se le indique
         //En este caso, almacena 2 doubles y
         //getObjeto1() representa un dato de la columna de las X en en archivo de entradas
@@ -63,89 +66,112 @@ public class LinearRegressionServiceImpl implements LinearRegressionService {
         
         
         
-        //Esto se lee así: Nodo que guardaun objeto tipo Dupla 
-        //que guarda (2) objetos tipo Double, 
-        //Nodo< Dupla<Double> > auxiliar;
-        //auxiliar = datos.getPrimero();
-        //while(!datos.finDeRecorrido(auxiliar) ){
-            //datoDelNodo = auxiliar.getObjeto();
-            /*
-            Aquí se hace algo con los valores de la dupla que pueden ser
-            accedidos usando:
+         //Esto se lee así: Nodo que guardaun objeto tipo Dupla 
+         //que guarda (2) objetos tipo Double, 
+         //Nodo< Dupla<Double> > auxiliar;
+         //auxiliar = datos.getPrimero();
+         //while(!datos.finDeRecorrido(auxiliar) ){
+         //datoDelNodo = auxiliar.getObjeto();
+         /*
+         Aquí se hace algo con los valores de la dupla que pueden ser
+         accedidos usando:
             
-            datoDelNodo.datoDelNodo.getObjeto1()
-            datoDelNodo.datoDelNodo.getObjeto2()
-            */
-            
+         datoDelNodo.datoDelNodo.getObjeto1()
+         datoDelNodo.datoDelNodo.getObjeto2()
+         */
             //auxiliar = auxiliar.getSiguiente();
         //}
         //Aquí llegamos al final del arreglo :(
-        
-        
         /*
-        La sugerencia es no calcular las cosas aquí, por el contrario crear funciones
-        tan modulares como sea posible, de tal manera que se puedan reutilizar al máximo.
+         La sugerencia es no calcular las cosas aquí, por el contrario crear funciones
+         tan modulares como sea posible, de tal manera que se puedan reutilizar al máximo.
         
-        Si creen que hace falta agregar algun otro método a la interfaz
-        que es implementada por esta clase, pueden hacerlo con libertad.
+         Si creen que hace falta agregar algun otro método a la interfaz
+         que es implementada por esta clase, pueden hacerlo con libertad.
         
-        Saludos :D
-        */
-        
-        
+         Saludos :D
+         */
         //Ejemplo de retorno, creado para que pase el TEST.
         //ORDEN: B0, B1, Rxy, R2
         //return new double[]{/*B0*/-22.55, /*B1*/1.7279, /*Rxy*/0.9545,  /*R2*/0.9111};
-        return new double[]{salidas.get(0),salidas.get(1),salidas.get(2),salidas.get(3)};
+        try{
+             resultado = new double[]{salidas.get(0), salidas.get(1), salidas.get(2), salidas.get(3)};
+        }catch (NullPointerException e){
+            throw new PSP2Exception("Some value couldn't be calculated.");
+        }
+                
+        return resultado;
     }
     
-    private void  sumatorias(ListaLigadaConCabeza<Dupla<Double>> datos){
+    private void sumatorias(ListaLigadaConCabeza<Dupla<Double>> datos) {
         
         Dupla<Double> datoDelNodo;
         cantidad = datos.getTamano();
         
-        Nodo< Dupla<Double> > auxiliar;
-        auxiliar = datos.getPrimero();
-        while(!datos.finDeRecorrido(auxiliar) ){
-            datoDelNodo = auxiliar.getObjeto();
-            sumatoriaX = sumatoriaX + datoDelNodo.getObjeto1();
-            sumatoriaY = sumatoriaY + datoDelNodo.getObjeto2();
-            sumatoriaXCuad = sumatoriaXCuad + Math.pow(datoDelNodo.getObjeto1(), 2);
-            sumatoriaYCuad = sumatoriaYCuad + Math.pow(datoDelNodo.getObjeto2(), 2);
-            sumatoriaXY = sumatoriaXY + (datoDelNodo.getObjeto1()*datoDelNodo.getObjeto2());
-            auxiliar = auxiliar.getSiguiente();
-        }
+        Nodo< Dupla<Double>> auxiliar;
         
-        promedioX = sumatoriaX / cantidad;
-        promedioY = sumatoriaY / cantidad;
+        if ((auxiliar = datos.getPrimero()) != null) {
+            while (!datos.finDeRecorrido(auxiliar)) {
+                datoDelNodo = auxiliar.getObjeto();
+                sumatoriaX = sumatoriaX + datoDelNodo.getObjeto1();
+                sumatoriaY = sumatoriaY + datoDelNodo.getObjeto2();
+                sumatoriaXCuad = sumatoriaXCuad + Math.pow(datoDelNodo.getObjeto1(), 2);
+                sumatoriaYCuad = sumatoriaYCuad + Math.pow(datoDelNodo.getObjeto2(), 2);
+                sumatoriaXY = sumatoriaXY + (datoDelNodo.getObjeto1() * datoDelNodo.getObjeto2());
+                auxiliar = auxiliar.getSiguiente();
+            }
+            
+            promedioX = sumatoriaX / cantidad;
+            promedioY = sumatoriaY / cantidad;
+        }
     }
     
-    public Double beta1(){
-        Double beta1;
-        Double numerador = sumatoriaXY - (cantidad * promedioX * promedioY);
-        Double denominador = sumatoriaXCuad - (cantidad * (Math.pow(promedioX, 2)));
-        beta1 = numerador / denominador;
-        return beta1;
+    public Double beta1() {
+        
+        if (cantidad != 0.0) {
+            Double beta1;
+            Double numerador = sumatoriaXY - (cantidad * promedioX * promedioY);
+            Double denominador = sumatoriaXCuad - (cantidad * (Math.pow(promedioX, 2)));
+            beta1 = numerador / denominador;
+            return beta1;
+        } else {
+            return 0.0;
+        }
     }
     
-     public Double beta0(){
-         Double beta2 = promedioY - (beta1()* promedioX);
-         return beta2;
-     }
-     
-     public Double rxy(){
-         Double rxy;
-         Double numerador = (cantidad *sumatoriaXY) - (sumatoriaX* sumatoriaY);
-         Double primFactor = (cantidad * sumatoriaXCuad) - Math.pow(sumatoriaX, 2);
-         Double segFactor = (cantidad * sumatoriaYCuad) - Math.pow(sumatoriaY, 2);
-         Double denominador = Math.sqrt(primFactor * segFactor);
-         rxy = numerador / denominador;
-         return rxy;
-     }
-     
-     public Double rCuadrado(){
-         Double rCuadrado = Math.pow(rxy(), 2);
-         return rCuadrado;
-     }    
-
+    public Double beta0() {
+        Double beta2 = promedioY - (beta1() * promedioX);
+        return beta2;
+    }
+    
+    public Double rxy() throws PSP2Exception {
+        
+        if (cantidad != 0.0) {
+            Double rxy;
+            Double numerador = (cantidad * sumatoriaXY) - (sumatoriaX * sumatoriaY);
+            Double primFactor = (cantidad * sumatoriaXCuad) - Math.pow(sumatoriaX, 2);
+            Double segFactor = (cantidad * sumatoriaYCuad) - Math.pow(sumatoriaY, 2);
+            
+            if (primFactor * segFactor > 0) {
+                rxy = numerador / Math.sqrt(primFactor * segFactor);
+                return rxy;
+            } else {
+                throw new PSP2Exception("Fail calculating square root.");
+            }
+        } else {
+            return 0.0;
+        }
+    }
+    
+    public Double rCuadrado() throws PSP2Exception {
+        Double rxy = rxy();
+        if (rxy != null) {
+            Double rCuadrado = Math.pow(rxy, 2);
+            return rCuadrado;
+        } else {
+            throw new PSP2Exception("Null X x Y.");
+            
+        }
+    }
+    
 }
--- PSP2/PSP2/src/test/java/co/edu/udea/pruebas/psp2/servlet/domain/operation/process/LinearRegressionServiceTest.java
+++ PSP2/PSP2/src/test/java/co/edu/udea/pruebas/psp2/servlet/domain/operation/process/LinearRegressionServiceTest.java
@@ -9,6 +9,8 @@ import co.edu.udea.pruebas.psp2.exception.PSP2Exception;
 import co.edu.udea.pruebas.psp2.servlet.domain.operation.process.impl.LinearRegressionServiceImpl;
 import co.edu.udea.pruebas.psp2.utils.llcc.Dupla;
 import co.edu.udea.pruebas.psp2.utils.llcc.ListaLigadaConCabeza;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -23,6 +25,7 @@ import static org.junit.Assert.*;
 public class LinearRegressionServiceTest {
     
     ListaLigadaConCabeza<Dupla<Double>> datosBien1;
+    ListaLigadaConCabeza<Dupla<Double>> sinDatos;
     
     public LinearRegressionServiceTest() {
     }
@@ -30,6 +33,7 @@ public class LinearRegressionServiceTest {
     
     @Before
     public void setUp() {
+        sinDatos = new ListaLigadaConCabeza<>();
         datosBien1 = new ListaLigadaConCabeza<>();
         datosBien1.insertar(new Dupla<>(130d,186d));
         datosBien1.insertar(new Dupla<>(650d,699d));
@@ -57,6 +61,35 @@ public class LinearRegressionServiceTest {
         assertArrayEquals(expResult, result,0.01);
     }
 
-    
+    @Test
+    public void testNoData()   {
+        System.out.println("getLinearRegressionParameters");
+        
+        LinearRegressionService instance = new LinearRegressionServiceImpl();
+        double[] expResult = new double[]{/*B0*/0 , /*B1*/0, /*Rxy*/0,  /*R2*/0};
+        double[] result;
+        try {
+            result = instance.getLinearRegressionParameters(sinDatos);
+            assertArrayEquals(expResult, result,0.01);
+        } catch (PSP2Exception ex) {
+            fail("lanzó excepción");
+        }
+        
+    }
+//    @Test
+//    public void testNegativeData()   {
+//        boolean threwException = false;
+//        System.out.println("getLinearRegressionParameters");
+//
+//        LinearRegressionService instance = new LinearRegressionServiceImpl();
+//        
+//        try{
+//            double[] result = instance.getLinearRegressionParameters(datosBien1);
+//        }catch(PSP2Exception e){
+//            assertEquals(e.getMessage(), "Fail calculating square root.");
+//            threwException = true;
+//        }
+//        assert(threwException);
+//    }
     
 }
